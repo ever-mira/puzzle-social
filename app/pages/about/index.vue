98f52cd6f63d90f944fa4921b347a0891b643d6a
Ra13um42 <to@outlook.de>
2025-02-16 21:01:41 +0100

    about page

--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -1,10 +1,62 @@
 <template>
-    <main class="p-20">
-        <h1 class="text-2xl">Über das Projekt</h1>
+    <Page>
+        <h1 class="text-3xl">Über das Projekt</h1>
         <br>
-        <h2 class="text-xl">Technical documentation at:</h2>
+
+        <h2 class="text-xl">Das Netzwerk soll gebaut werden von den Menschen, die es nutzen wollen.</h2>
+        <br><br>
+        <h2 class="text-xl">Welche Features wollen wir? Wenn alles möglich wäre?</h2>
+
+        <br><br>
+        <h2 class="text-xl">Folgende Aufgabenbereiche könnte es geben:</h2><br>
+        <ul class="list-disc list-inside">
+            <li>Ausdenken der Features - Community Interaktion</li>
+            <li>Entwicklung</li>
+            <li>Design</li>
+            <li>Test</li>
+            <li>Deployment - Infrastruktur</li>
+            <li>Öffentlichkeitsarbeit - Networking</li>
+            <li>Support / Help</li>
+            <li>Moderation</li>
+        </ul>
+
+        <br>
+        <h2 class="text-xl">Jeder der das Projekt unterstützen will, kann einfach der Gruppe
+            beitreten:</h2>
+        <a href="https://t.me/puzzle_social_team" target="_blank"
+            class="text-blue-600">https://t.me/puzzle_social_team</a><br>
+        (Und einfach etwas in die Gruppe schreiben, wie z.B: "ich finde die Idee gut")<br>
+        <br>
+
+
+        <br>
+        <h2 class="text-xl">Hier gibt es den Contribution Guide:</h2>(eher technisch und für Programmierer)<br>
         <a href="https://puzzle.codes" target="_blank" class="text-blue-600">https://puzzle.codes</a>
         <br><br>
+        Das Projekt ist Open Source, d.h. jeder kann sich den Code anschauen (siehe Contribution Guide) und selbst zum
+        Code beitragen (Features implementieren). Gesucht werden Visionäre, die programmieren können, und Träumer,
+        die sich die Funktionen ausdenken.<br><br>
+
+        <br><br>
+        <h2 class="text-xl">Unterstützung</h2>
+        Jede Art von Weitersagen / Networking, oder einfach nur gute Ideen, und Vorschläge.<br><br>
+
+
+        <h2 class="text-xl">Erfolg</h2>
+        Das Projekt ist erfolgreich, wenn wir Lust bekommen, selbst auf der Plattform zu
+        stöbern / zu interagieren.
+        Wenn nicht, wollen wir uns überlegen, was noch fehlt, damit sich das ändert.<br><br>
+
+        <h2 class="text-xl">Möglichkeiten</h2>
+        Aus allen unendlichen Möglichkeiten, welche denkbaren Funktionalitäten, die wir in zukünftigen Realitäten
+        entwickelt haben, haben das Leben auf der Erde deutlich verbessert? <br><br>
+
+        <h2 class="text-xl">Energie für das Projekt</h2>
+        Lebendigkeit auf der Plattform<br><br>
+
+        <br><br><br>
+
+        Technical details in short:
         <h2 class="text-xl">built with Nuxt</h2>
         A meta-framework based on Vue.js
         <h2 class="text-xl">built with Supabase</h2>
@@ -13,8 +65,11 @@
             <img src="assets/supabase.png" class="mx-auto">
             <img src="assets/supabase.jpg" class="mx-auto">
         </div>
-    </main>
+    </Page>
 </template>
 
-<script>
+<script setup lang="ts">
+useHead({
+    title: 'Puzzle About',
+})
 </script>
